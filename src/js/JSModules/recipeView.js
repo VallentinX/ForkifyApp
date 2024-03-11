91783cbb4e5cee5fbdaa43840acaf58e6fd6fc76
VallentinX <valentin.cristian_98@icloud.com>
2024-03-11 20:27:59 +0200

    7th commit

--- src/js/JSModules/recipeView.js
+++ src/js/JSModules/recipeView.js
@@ -1,4 +1,4 @@
-import View from './view.js';
+import View from './views.js';
 import icons from 'url:../../img/icons.svg';
 import { Fraction } from 'fractional';
 
--- src/js/JSModules/resaultView.js
+++ src/js/JSModules/resaultView.js
@@ -1,4 +1,4 @@
-import View from './view.js';
+import View from './views.js';
 import previewView from './previewView.js';
 import icons from 'url:../../img/icons.svg';
 
